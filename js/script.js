3b1308a8b2a1820041bdd723274ad29647582826
maemochi <maemoch15@gmail.com>
2022-07-04 07:16:09 +0900

    ハンバーガーボタン作成

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(".p-header__button").click(function () {
+  $(this).toggleClass("open");
+});
+
+//===========jQuery作動のチェック用=============
+// $(function () {
+//   alert("jQueryが正常に動作しています！");
+// });
