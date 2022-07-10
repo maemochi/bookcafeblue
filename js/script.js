e020b718476b02bd741da249745b186a1161ab17
maemochi <maemoch15@gmail.com>
2022-07-10 18:50:19 +0900

    アニメーション設定

--- js/script.js
+++ js/script.js
@@ -4,6 +4,31 @@ $(".p-header__button").click(function () {
   $(".c-cover__overlay").toggleClass("active");
 });
 
+$(".p-header a").click(function () {
+  $(".p-header__button").removeClass("open");
+  $(".p-header__nav").removeClass("active");
+  $(".c-cover__overlay").removeClass("active");
+});
+
+$(window).scroll(function () {
+  $(".slideUp").each(function () {
+    const scroll = $(window).scrollTop();
+    const blockPosition = $(this).offset().top;
+    const windowHeight = $(window).height();
+    if (scroll > blockPosition - windowHeight + 100) {
+      $(this).addClass("move");
+    }
+  });
+});
+$(window).scroll(function () {
+  const stickyHeight = $(".p-content-about").offset().top;
+  if ($(this).scrollTop() > stickyHeight) {
+    $(".p-header__top").addClass("sticky");
+  } else {
+    $(".p-header__top").removeClass("sticky");
+  }
+});
+
 //===========jQuery作動のチェック用=============
 // $(function () {
 //   alert("jQueryが正常に動作しています！");
