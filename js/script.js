c9df85de26221322bd8f62f70feddc5bce9b5431
maemochi <maemoch15@gmail.com>
2022-07-17 16:03:42 +0900

    ページ内リンク時の動き調整

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+//ヘッダーボタンをクリックした時の処理
 $(".p-header__button").click(function () {
   $(this).toggleClass("open");
   $(".p-nav").toggleClass("active");
   $(".c-cover__overlay").toggleClass("active");
   $("body").css("overflow", "hidden");
 });
+//navボタンをクリックした時の処理
 $(".p-nav__button").click(function () {
   $(".p-header__button").removeClass("open");
   $(".p-nav").removeClass("active");
@@ -11,6 +13,7 @@ $(".p-nav__button").click(function () {
   $("body").css("overflow", "visible");
 });
 
+// nav内リンクをクリックした時の処理
 $(".p-nav a").click(function () {
   $(".p-header__button").removeClass("open");
   $(".p-nav").removeClass("active");
@@ -18,6 +21,7 @@ $(".p-nav a").click(function () {
   $("body").css("overflow", "visible");
 });
 
+// スクロール時のアニメーション(ふわっとスライド表示)
 $(window).scroll(function () {
   $(".slideUp").each(function () {
     const scroll = $(window).scrollTop();
@@ -30,6 +34,7 @@ $(window).scroll(function () {
     }
   });
 });
+// スクロール後、ヘッダーを固定表示
 $(window).scroll(function () {
   const stickyHeight = $(".l-container").offset().top;
   if ($(this).scrollTop() > stickyHeight) {
@@ -37,6 +42,18 @@ $(window).scroll(function () {
   } else {
     $(".p-header__top").removeClass("sticky");
   }
+});
+
+// ヘッダー固定時のアンカーリンク位置ずれ調整
+$(function () {
+  const headerHight = 100;
+  $("a[href^='#']").click(function () {
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const position = target.offset().top - headerHight;
+    $("html, body").animate({ scrollTop: position }, 300, "linear");
+    return false;
+  });
 });
 
 //===========jQuery作動のチェック用=============
